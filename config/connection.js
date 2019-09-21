4e6b24444d00159aeaafbe4a227e7d3aa7c68f79
syeda zaidi <zaidisyeda03@gmail.com>
2019-09-21 13:16:05 -0700

    added git ignore

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
+  // Database is JawsDB on Heroku
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
