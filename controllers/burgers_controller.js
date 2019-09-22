f11085737904a1685bf2fea1c9e4b601c9df964f
syeda zaidi <zaidisyeda03@gmail.com>
2019-09-22 00:14:45 -0700

    changed pot method to make post from form and not js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -30,21 +30,18 @@ router.post("/api/burgers", function(req, res) {
     });
 });
 
-router.put("/api/burgers/:id", function(req,res) {
+router.put("/burgers/:id", function(req,res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition : " + condition);
 
   burgersModel.updateOne(
     { 
-      devoured: req.body.devoured
+      devoured: true
     }, 
     condition, 
     function(result){
-      if (result.changedRows === 0 ) {
-        return res.status(404).end();
-      } 
-      res.status(200).end();
+      res.redirect("/");
   });
 });
 
--- public/assets/js/burgerscs.js
+++ public/assets/js/burgerscs.js
@@ -3,33 +3,33 @@ console.log("loaded js")
 $(document).ready(function() {
 
 
-    $("#form-id").on("submit", function(event) {
+    // $("#form-id").on("submit", function(event) {
         
-        event.preventDefault();
+    //     event.preventDefault();
 
-        var newBurger = {
-            burger_name: $("#burger-input").val().trim(),
-            devoured: 0
-        };
-        console.log(newBurger);
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(function() {
-            console.log("added new burger");
+    //     var newBurger = {
+    //         burger_name: $("#burger-input").val().trim(),
+    //         devoured: 0
+    //     };
+    //     console.log(newBurger);
+    //     $.ajax("/api/burgers", {
+    //         type: "POST",
+    //         data: newBurger
+    //     }).then(function() {
+    //         console.log("added new burger");
             
-            location.reload();
-        });
-    });
+    //         location.reload();
+    //     });
+    // });
 
     //add eatburger click evt to change devoured state from 0 to 1 
 
-    $(".change-to-devoured").on("click", function(event) {
+    $(".to-be-devoured").on("click", function(event) {
         var id = $(this).data("id");
         var newSTATE = $(this).data("nowDevoured");
 
         var newBurgerState = {
-            devoured = true
+            devoured: true
         };
 
         $.ajax("/api/burgers" + id, {
